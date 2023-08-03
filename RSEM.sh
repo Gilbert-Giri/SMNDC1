140ef78a0d4331bf19eb1b4236c80b95d315aa7d
Gilbert-Giri <111405438+Gilbert-Giri@users.noreply.github.com>
2023-08-03 15:23:20 -0400

    Create RSEM.sh
    RSEM Quantification

---
+++ RSEM.sh
@@ -0,0 +1,13 @@
+index="Human/RSEM/hg38"
+bamdir="../STAR_mapped"
+outdir="../RSEM"
+file=$1
+
+rsem-calculate-expression --paired-end \
+        --num-threads 32 \
+        --alignments \
+        --seed 77 \
+        --no-bam-output \
+        ${bamdir}/${file}Aligned.toTranscriptome.out.bam \
+        ${index} \
+        ${outdir}/${file}
